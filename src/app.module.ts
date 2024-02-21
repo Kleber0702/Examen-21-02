dc0145247942871b21903ae8fddcafa1c4305f18
Kleber0702 <128488482+Kleber0702@users.noreply.github.com>
2024-02-20 22:37:23 -0600

    Create app

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { IndexController } from './index/index.controller';
+
+@Module({
+  imports: [],
+  controllers: [IndexController],
+  providers: [AppService],
+})
+export class AppModule {}
